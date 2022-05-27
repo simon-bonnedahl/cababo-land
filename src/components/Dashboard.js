1b5587f3aa81d522b06b0088cf947ca46361a53b
Simon Bonnedahl <simonbonnedahl@simons-mbp.lan>
2022-05-27 13:54:24 +0200

    Dashboard early stage

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,31 +1,65 @@
 
 import './Dashboard.css'
-const Dashboard = ({db, account}) => {
+const Dashboard = ({db, account, web3Handler}) => {
     /*<h1>Total CBOTokens</h1>
             <h1>Minted CBOTokens</h1>
             <h1>Unminted CBOTokens</h1>
             <button>Mint CBOTokens to wallet</button>
             <h1>Average hourly income</h1>
-            <h1>My Plots</h1>
-            <h1>CBOToken value</h1>*/
+            */
     return (
-        <div className="main">
-            <div className="top">
+        <div className="body">
+            
+        <nav className="flex-between">
+        {account ? (
+                <a
+                    href={`${""}/address/${account}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="button">
+                    {account.slice(0, 5) + '...' + account.slice(38, 42)}
+                </a>
+            ) : (
+                <button onClick={web3Handler} className="button">Connect Wallet</button>
+            )}
 
-            </div>
+        </nav>
+        <div className="main">
             <div className="upper">
+                <div className="left">
+                <div className="section-w32 round-border center-text opacity-10">
+                <h1 className="center-text">CBO balance</h1>
+                <p>Unminted CBOTokens: 0</p>
+                <button className="button-small">Mint CBOTokens</button>
+                <p>Minted CBOTokens: 0</p>
                 
-            </div>
-            <div className="mid">
+                <p>Total CBOTokens: 0</p>
+                </div>
+                <div className="section-w32 round-border opacity-10">
+                <h1 className="center-text">Sales</h1>
+                </div>
+                <div className="section-w32 round-border opacity-10">
+                <h1 className="center-text">Plots</h1>
+                </div>
+                </div>
+                <div className="right">
+                <div className="section-h40 round-border opacity-10 center-text">
+                    <h1>CBOToken value</h1>
+                </div>
+                <div className="section-h25 round-border opacity-10"></div>
+                <div className="section-h25 round-border opacity-10"></div>
+                </div>
+                
                 
             </div>
             <div className="lower">
-                
-            </div>
-            <div className="bot">
-                
+            <div className="left round-border opacity-10">
+                </div>
+                <div className="right round-border opacity-10">
+                </div>
             </div>
             
+        </div>
         </div>
     )
 }
