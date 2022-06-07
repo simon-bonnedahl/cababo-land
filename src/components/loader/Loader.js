def457cace423b65f3cd6f855c9fcd736b0b54dd
Simon Bonnedahl <simbo803@student.liu.se>
2022-06-07 17:18:06 +0200

    Interface updates

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -7,19 +7,13 @@ const Loader = () => {
           <div className="cell d-0"></div>
           <div className="cell d-1"></div>
           <div className="cell d-2"></div>
-          <div className="cell d-3"></div>
           <div className="cell d-1"></div>
           <div className="cell d-2"></div>
           <div className="cell d-3"></div>
-          <div className="cell d-4"></div>
           <div className="cell d-2"></div>
           <div className="cell d-3"></div>
           <div className="cell d-4"></div>
-          <div className="cell d-5"></div>
-          <div className="cell d-3"></div>
-          <div className="cell d-4"></div>
-          <div className="cell d-5"></div>
-          <div className="cell d-6"></div>
+
         </div>
     )
 }
